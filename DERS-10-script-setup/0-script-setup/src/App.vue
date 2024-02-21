869f4ba03201a70a480246a82e4caee2df28b8fc
ssule <ssulecelepp@gmail.com>
2024-02-21 16:12:59 +0300

    Composition API TodoList Componentlere Ayrıldı ve Tamamlandı. VueJs 3, Composition API, script setup, ref, reactive, watch, computed, composable, defineProps, defineEmits, provide, inject kullanımı Fatura Uygulamasında kullanıldı.

---
+++ DERS-10-script-setup/0-script-setup/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h3>{{ title }}</h3>
+  <input type="text" v-model="title">
+  <button @click="inc">{{ counter }}</button>
+  <hr>
+  <oddOrEven :counter="counter" @odd-event="alertMe"/>
+  <hr>
+  <h1>User App</h1>
+  <input type="text" v-model="data.personal.name">
+  <input type="text" v-model="data.personal.lname">
+  {{ data.personal }}
+</template>
+
+<script setup>
+import {ref, reactive,watch} from "vue";
+import oddOrEven from "./components/oddOrEven.vue";
+import Utils from "./composables/Utils.js";
+
+const { title, counter, inc, alertMe }=Utils();
+
+// User App
+// const name= ref("");
+// const lname= ref(""); //bu şekilde ref tanımlanması tercih edilmiyor
+const data= reactive({
+  personal:{
+    name:null,
+    lname:null,
+  },
+});
+
+// () =>JSON.parse(JSON.stringify(state.personal))
+watch(() =>JSON.parse(JSON.stringify(data.personal)), (newPersonal,oldPersonal)=>{
+  console.log(oldPersonal);
+  console.log(newPersonal);
+  
+})
+
+
+</script>
---
+++ DERS-10-script-setup/0-script-setup/src/components/oddOrEven.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>aaa</h1>
+  <h3>{{ result }}</h3>
+  {{ counter }}
+</template>
+
+<script setup>
+import { computed, watch } from "vue";
+const props = defineProps({ counter: Number });
+const emit = defineEmits(["odd-event"]);
+
+const result = computed(() => {
+  return props.counter % 2 == 0 ? "Çift" : "Tek";
+});
+
+//tek olduğunda event gönderelim
+watch(result, (result) => {
+  if (result === "Tek") {
+     emit("odd-event", true);
+  }
+});
+</script>
